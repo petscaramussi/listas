95e91d76901394fd33a5aeb21c0e5432c7f8e1af
Peterson <44592787+petscaramussi@users.noreply.github.com>
2021-04-24 16:51:37 -0300

    alert dialog add

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   List _itens = [];
 
-  void _carregarItens(){
-
+  void _carregarItens() {
     _itens = [];
 
-    for(int i = 0; i <= 10; i++){
+    for (int i = 0; i <= 10; i++) {
       Map<String, dynamic> item = Map();
       item["titulo"] = "Titulo $i daejdijaejiadeijdae";
       item["descricao"] = "Descrição $i dajidejaajied";
-      _itens.add( item );
-
+      _itens.add(item);
     }
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
     _carregarItens();
 
     return Scaffold(
@@ -36,14 +30,49 @@ class _HomeState extends State<Home> {
       body: Container(
         padding: EdgeInsets.all(20),
         child: ListView.builder(
-          itemCount: _itens.length,
-          itemBuilder: (context, indice){
-            return ListTile(
-              title: Text( _itens[indice]["titulo"] ),
-              subtitle: Text( _itens[indice]["descricao"] ),
-            );
-          }
-        ),
+            itemCount: _itens.length,
+            itemBuilder: (context, indice) {
+              return ListTile(
+                onTap: (){
+                  showDialog(
+                    context: context,
+                    builder: (context){
+                      return AlertDialog(
+                        title: Text( _itens[indice]["titulo"] ),
+                        titlePadding: EdgeInsets.all(20),
+                        titleTextStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.orange
+                        ),
+                        content: Text( _itens[indice]["descricao"] ),
+                        actions: <Widget>[
+                          FlatButton(
+                            color: Colors.green, 
+                            onPressed: (){
+                              print("selecionado sim");
+                              Navigator.pop(context);
+                            },
+                            child: Text("sim"),
+                          ),
+                            FlatButton(
+                            color: Colors.red, 
+                            onPressed: (){
+                              print("selecionado não");
+                              Navigator.pop(context);
+                            },
+                            child: Text("não"),
+                            
+                          )
+                        ],
+                        //backgroundColor: Colors.red,
+                      );
+                    } 
+                  );
+                },
+                title: Text( _itens[indice]["titulo"] ),
+                subtitle: Text( _itens[indice]["descricao"] ),
+              );
+            }),
       ),
     );
   }
